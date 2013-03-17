0be9f40a1c85637f5aea67973d8045ba34cc35dc
Nathan Villaescusa <nathanv@minted.com>
2013-03-17 15:43:10 -0700

    Add a setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os.path
+from setuptools import setup
+
+setup(
+    name='Third Panel',
+    version='0.0.1',
+    packages=['thirdpanel'],
+    author='Nathan Villaescusa',
+    author_email='nathan@signedzero.com',
+    description='Server for webcomics',
+    url='http://github.com/signed0/thirdpanel',
+    license='BSD',
+    entry_points={},
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Operating System :: OS Independent',
+                 'License :: OSI Approved :: BSD License'
+                 ],
+    install_requires=[],
+    zip_safe=True,
+)
